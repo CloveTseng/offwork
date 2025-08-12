272be5e6143ce3558b03917163d4c11972540bf9
Tippy Wang <ektodorwang@gmail.com>
2025-08-12 20:51:13 +0800

    fix: 🐛 抽出 Bottom Bar

---
+++ app/components/Layout/BottomBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <!-- 底部橫杠 -->
+  <div class="flex justify-center">
+    <div
+      class="mb-2 mt-[27px] h-[5px] w-[134px] rounded-[100px] bg-neutral"
+    ></div>
+  </div>
+</template>
