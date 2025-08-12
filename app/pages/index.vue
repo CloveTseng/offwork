3f6d2e81f63faa5bab3742a773f9824c8c90f4bf
Aaron <piafyoyo06@gmail.com>
2025-08-12 09:47:30 +0800

    feat: 首頁假內容

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,43 @@
 <template>
-  <h1 class="text-accent text-3xl">首頁</h1>
+  <h1 class="text-3xl text-accent">首頁</h1>
+  <p class="text-neutral">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio similique
+    optio officia qui assumenda facilis voluptate, eum excepturi nam iusto
+    molestiae incidunt soluta suscipit quia quae sint saepe explicabo odit
+    exercitationem recusandae atque veniam ullam accusamus distinctio.
+    Voluptatum cum quasi asperiores esse neque laudantium saepe? Officia nemo
+    totam dolore voluptatem!
+  </p>
+  <p class="text-neutral">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio similique
+    optio officia qui assumenda facilis voluptate, eum excepturi nam iusto
+    molestiae incidunt soluta suscipit quia quae sint saepe explicabo odit
+    exercitationem recusandae atque veniam ullam accusamus distinctio.
+    Voluptatum cum quasi asperiores esse neque laudantium saepe? Officia nemo
+    totam dolore voluptatem!
+  </p>
+  <p class="text-neutral">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio similique
+    optio officia qui assumenda facilis voluptate, eum excepturi nam iusto
+    molestiae incidunt soluta suscipit quia quae sint saepe explicabo odit
+    exercitationem recusandae atque veniam ullam accusamus distinctio.
+    Voluptatum cum quasi asperiores esse neque laudantium saepe? Officia nemo
+    totam dolore voluptatem!
+  </p>
+  <p class="text-neutral">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio similique
+    optio officia qui assumenda facilis voluptate, eum excepturi nam iusto
+    molestiae incidunt soluta suscipit quia quae sint saepe explicabo odit
+    exercitationem recusandae atque veniam ullam accusamus distinctio.
+    Voluptatum cum quasi asperiores esse neque laudantium saepe? Officia nemo
+    totam dolore voluptatem!
+  </p>
+  <p class="text-neutral">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio similique
+    optio officia qui assumenda facilis voluptate, eum excepturi nam iusto
+    molestiae incidunt soluta suscipit quia quae sint saepe explicabo odit
+    exercitationem recusandae atque veniam ullam accusamus distinctio.
+    Voluptatum cum quasi asperiores esse neque laudantium saepe? Officia nemo
+    totam dolore voluptatem!
+  </p>
 </template>
