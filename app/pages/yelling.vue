3fb83a46a7e5d31c7380f90708f8d05bbf0d1a78
Aaron <piafyoyo06@gmail.com>
2025-08-15 16:13:08 +0800

    feat: 大吼中區塊

--- app/pages/yelling.vue
+++ app/pages/yelling.vue
@@ -19,14 +19,34 @@ const isYelling = ref(false);
   </h1>
   <!-- 大吼中 -->
   <main v-if="isYelling">
-    <Lottie name="yelling/volume" />
+    <img
+      src="/images/yelling/yelling-red-volcano.gif"
+      alt="大吼中紅色火山君"
+      class="pointer-events-none absolute bottom-[104px] left-1/2 z-0 w-full max-w-[375px] -translate-x-1/2"
+    />
+    <!-- 音量 Lottie、停止按鈕 -->
+    <div
+      class="absolute bottom-0 left-1/2 z-10 flex w-full -translate-x-1/2 flex-col items-center bg-gradient-to-t from-neutral-950 from-40% to-[#29292D00] pt-8"
+    >
+      <Lottie name="yelling/volume" />
+      <!-- 停止按鈕 -->
+      <div class="mb-10 rounded-full border-[11px] border-[#C0C7DC33]">
+        <NuxtLink
+          :to="{ path: '/', query: { openFeelingBetter: 'true' } }"
+          class="block rounded-full bg-neutral-950 p-6"
+        >
+          <img src="/icons/yelling/stop-fill.svg" alt="停止 icon" />
+        </NuxtLink>
+      </div>
+      <LayoutBottomBar class="mb-2 mt-[27px]" />
+    </div>
   </main>
   <!-- 準備大吼 -->
   <main v-else>
     <img
       src="/images/yelling/before-yelling-red-volcano.webp"
       alt="準備大吼紅色火山君"
-      class="pointer-events-none absolute bottom-[104px] left-1/2 z-0 w-full -translate-x-1/2"
+      class="pointer-events-none absolute bottom-[104px] left-1/2 z-0 w-full max-w-[375px] -translate-x-1/2"
     />
     <!-- 靜態音量圖片、開始按鈕 -->
     <div
@@ -34,7 +54,7 @@ const isYelling = ref(false);
     >
       <img src="/images/yelling/volume.webp" alt="音量圖片" class="mb-8" />
       <!-- 開始按鈕 -->
-      <div class="mx-auto mb-10 rounded-full border-[11px] border-[#C0C7DC33]">
+      <div class="mb-10 rounded-full border-[11px] border-[#C0C7DC33]">
         <button
           type="button"
           class="rounded-full bg-white p-6"
