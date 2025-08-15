3bfbc5db3961cd8a6ff583e4f2410867c4de837b
Aaron <piafyoyo06@gmail.com>
2025-08-15 16:05:35 +0800

    feat: 準備大吼切版

--- app/pages/yelling.vue
+++ app/pages/yelling.vue
@@ -3,6 +3,47 @@ useSeoMeta({
   title: "大吼 | 健康生活 OFFWORK APP",
   ogTitle: "大吼 | 健康生活 OFFWORK APP",
 });
+
+const isYelling = ref(false);
 </script>
 
-<template><h1>大吼</h1></template>
+<template>
+  <h1 class="relative py-2.5 text-center text-xl font-bold text-white">
+    <NuxtLink
+      :to="{ path: '/', query: { openCeremony: 'true' } }"
+      class="absolute left-0 top-1/2 block -translate-y-1/2 py-2 pl-4 pr-2"
+    >
+      <img src="/icons/white-left-arrow.svg" alt="返回箭頭" />
+    </NuxtLink>
+    放聲大吼吧
+  </h1>
+  <!-- 大吼中 -->
+  <main v-if="isYelling">
+    <Lottie name="yelling/volume" />
+  </main>
+  <!-- 準備大吼 -->
+  <main v-else>
+    <img
+      src="/images/yelling/before-yelling-red-volcano.webp"
+      alt="準備大吼紅色火山君"
+      class="pointer-events-none absolute bottom-[104px] left-1/2 z-0 w-full -translate-x-1/2"
+    />
+    <!-- 靜態音量圖片、開始按鈕 -->
+    <div
+      class="absolute bottom-0 left-1/2 z-10 flex w-full -translate-x-1/2 flex-col items-center bg-gradient-to-t from-neutral-950 from-50% to-[#29292D00] pt-8"
+    >
+      <img src="/images/yelling/volume.webp" alt="音量圖片" class="mb-8" />
+      <!-- 開始按鈕 -->
+      <div class="mx-auto mb-10 rounded-full border-[11px] border-[#C0C7DC33]">
+        <button
+          type="button"
+          class="rounded-full bg-white p-6"
+          @click="isYelling = true"
+        >
+          <img src="/icons/yelling/mic.svg" alt="麥克風 icon" />
+        </button>
+      </div>
+      <LayoutBottomBar class="mb-2 mt-[27px]" />
+    </div>
+  </main>
+</template>
