df5c0a9cba9f3e82a62a145f13117c0d9e40abc0
Aaron <piafyoyo06@gmail.com>
2025-08-25 00:47:26 +0800

    feat: Modal 元件

---
+++ app/components/Common/Modal.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+defineProps({
+  /**
+   * 是否顯示
+   * @type {Boolean}
+   */
+  show: {
+    type: Boolean,
+    default: false,
+  },
+});
+const emit = defineEmits<{
+  (e: "update:show", value: boolean): void;
+}>();
+
+const close = () => {
+  emit("update:show", false);
+};
+</script>
+
+<template>
+  <ClientOnly>
+    <Teleport to="#app-overlay-root">
+      <!-- Backdrop -->
+      <Transition name="fade">
+        <div
+          v-if="show"
+          class="absolute inset-0 z-40 bg-[#16161699]"
+          :class="show ? 'pointer-events-auto' : 'pointer-events-none'"
+          @click="close"
+        ></div>
+      </Transition>
+
+      <!-- Modal 本體 -->
+      <Transition name="fade">
+        <div
+          v-if="show"
+          class="Noto-Fonts | absolute left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2"
+          :class="show ? 'pointer-events-auto' : 'pointer-events-none'"
+        >
+          <slot />
+        </div>
+      </Transition>
+    </Teleport>
+  </ClientOnly>
+</template>
+
+<style scoped>
+.Noto-Fonts {
+  font-family: "Noto Sans TC", sans-serif;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
