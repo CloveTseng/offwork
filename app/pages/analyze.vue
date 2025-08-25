06e5874cf98d427c4336e06759e9e669ad65d3a6
CloveTseng <jessica.turn613@gmail.com>
2025-08-25 14:22:55 +0800

    fix: 修改原 title 改套用 deinePageMeta

--- app/pages/analyze.vue
+++ app/pages/analyze.vue
@@ -3,22 +3,27 @@ useSeoMeta({
   title: "分析 | 健康生活 OFFWORK APP",
   ogTitle: "分析 | 健康生活 OFFWORK APP",
 });
+definePageMeta({
+  header: {
+    // backTo: { path: "/", query: { openCeremony: "true" } },
+    title: "分析",
+  },
+});
 </script>
 <template>
   <main class="bg-neutral-950 px-4 grid gap-4">
     <div class=" text-white">
-      <h3 class="text-xl font-bold leading-[1.4] text-center py-[10px]">分析</h3>
       <!-- 日期及日曆 -->
       <section class="flex justify-between items-center">
-        <div class="bg-[#333339] rounded-full p-2 text-center">
+        <NuxtLink to="/" class="bg-[#333339] rounded-full p-2 text-center transition">
           <img src="/icons/white-left-arrow.svg" alt="左箭頭" />
-        </div>
+        </NuxtLink>
         <div class="bg-[#333339] rounded-full py-2 px-4 text-center">
           <p class="text-[14px] leading-relaxed font-bold">7/17</p>
         </div>
-        <div class="bg-[#333339] rounded-full p-2 text-center">
+        <NuxtLink to="#" class="bg-[#333339] rounded-full p-2 text-center">
           <img src="/icons/right-arrow.svg" alt="左箭頭" />
-        </div>
+        </NuxtLink>
       </section>
       <!-- 爆發指數 -->
       <section></section>
