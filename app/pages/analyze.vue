dfd198fa9a3fcbe5bf660b41eeb99c98027c9d1e
CloveTseng <jessica.turn613@gmail.com>
2025-08-25 13:58:22 +0800

    feat: 新增分析區塊

--- app/pages/analyze.vue
+++ app/pages/analyze.vue
@@ -6,24 +6,28 @@ useSeoMeta({
 </script>
 <template>
   <main class="bg-neutral-950 px-4 grid gap-4">
-      <div class=" text-white">
-        <h3 class="text-xl font-bold leading-[1.4] text-center py-[10px]">分析</h3>
-        <div class="flex justify-between items-center">
-          <div class="bg-[#333339] rounded-full p-2 text-center">
-            <img src="/icons/white-left-arrow.svg" alt="左箭頭" />
-          </div>
-          <div class="bg-[#333339] rounded-full py-2 px-4 text-center">
-            <p class="text-[14px] leading-relaxed font-bold">7/17</p>
-          </div>
-          <div class="bg-[#333339] rounded-full p-2 text-center">
-            <img src="/icons/right-arrow.svg" alt="左箭頭" />
-          </div>
+    <div class=" text-white">
+      <h3 class="text-xl font-bold leading-[1.4] text-center py-[10px]">分析</h3>
+      <!-- 日期及日曆 -->
+      <section class="flex justify-between items-center">
+        <div class="bg-[#333339] rounded-full p-2 text-center">
+          <img src="/icons/white-left-arrow.svg" alt="左箭頭" />
         </div>
-      </div>
-      <!-- nav -->
-      <div class="sticky inset-x-0 bottom-2">
-        <LayoutNavBar />
-        <LayoutBottomBar class="mb-2 mt-[27px]" />
-      </div>
+        <div class="bg-[#333339] rounded-full py-2 px-4 text-center">
+          <p class="text-[14px] leading-relaxed font-bold">7/17</p>
+        </div>
+        <div class="bg-[#333339] rounded-full p-2 text-center">
+          <img src="/icons/right-arrow.svg" alt="左箭頭" />
+        </div>
+      </section>
+      <!-- 爆發指數 -->
+      <section></section>
+      <div></div>
+    </div>
+    <!-- nav -->
+    <div class="sticky inset-x-0 bottom-2">
+      <LayoutNavBar />
+      <LayoutBottomBar class="mb-2 mt-[27px]" />
+    </div>
   </main>
 </template>
